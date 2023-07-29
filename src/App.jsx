ebc03f2eeac22bcd0823901045c9c1ef630d9532
MD MOMINUL ISLAM <mdmominulislam167@gmail.com>
2023-07-30 00:01:09 +0600

    3rd Commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,30 +12,30 @@ import ThemeContext from './contexts/ThemeContext'
 class App extends React.Component {
   state = {
     theme: "dark",
+    switchTheme : () =>{
+      this.setState(({theme}) =>{
+        if (theme === 'dark'){
+          return {
+            theme: "light"
+          }
+        } else {
+          return {
+            theme: 'dark'
+          }
+        }
+      })
+    },
   };
 
-  switchTheme = () =>{
-    this.setState(({theme}) =>{
-      if (theme === 'dark'){
-        return {
-          theme: "light"
-        }
-      } else {
-        return {
-          theme: 'dark'
-        }
-      }
-    })
-  }
+  
 
   render(){
-    const {theme} = this.state;
     return (
       <div className = "app">
         <Counter>
             {(count,incrementCount)=> (<ClickCounter count={count} incrementCount ={incrementCount}/>)}
         </Counter>
-        <ThemeContext.Provider value = {{theme, switchTheme: this.switchTheme}}><Section/></ThemeContext.Provider>
+        <ThemeContext.Provider value = {this.state}><Section/></ThemeContext.Provider>
         {/* <ThemeContext.Provider value = {{theme}}><Content/></ThemeContext.Provider> */}
       </div>
     )
